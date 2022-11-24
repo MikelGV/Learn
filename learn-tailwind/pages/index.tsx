9cdf9e825a01240bda09b1003f08af0a49d2008f
Mikel <mikelgaldosv@gmail.com>
2022-11-24 15:46:05 +0100

    tailwind first test

--- learn-tailwind/pages/index.tsx
+++ learn-tailwind/pages/index.tsx
@@ -5,7 +5,13 @@ import Layout from '../components/Layout'
 export default function Home() {
   return (
     <Layout home>
-      <h3 className='text-4xl font-normal'>This is index</h3>
+      <div className='p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4'>
+        <div className='shrink-0'>
+          <img className='h-12 w-12' src="/img/logo.svg" alt="ChitChat Logo" />
+        </div>
+        <div className='text-xl font-medium text-black'>ChitChat</div>
+        <p className='text-slate-500'>You have a new message!</p>
+      </div>
     </Layout>
   )
 }
