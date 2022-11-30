b8b3f6a91a5c38898109b398c59555c826fd52bb
Mikel <mikelgaldosv@gmail.com>
2022-11-30 01:47:21 +0100

    footer 1

--- learn-tailwind/components/Footer.tsx
+++ learn-tailwind/components/Footer.tsx
@@ -1,7 +1,11 @@
 export default function Footer() {
     return(
         <>
-            <h1>This is footer</h1>
+            <div className="p-6 max-w-full mx-auto bg-gray-200 shadow-lg flex items-stretch space-x-4 font-bebasneue">
+                <div className="shrink-0">
+                    <h1>Footer</h1>
+                </div>
+            </div>
         </>
     )
 }
