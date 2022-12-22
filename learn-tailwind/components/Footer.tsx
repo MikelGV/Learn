f8702c34313353d996d785fc3d835c4be955a3d3
Mikel <mikelgaldosv@gmail.com>
2022-12-22 04:19:39 +0100

    testing footer

--- learn-tailwind/components/Footer.tsx
+++ learn-tailwind/components/Footer.tsx
@@ -1,16 +1,13 @@
 export default function Footer() {
     return(
         <>
-            <div className="p-6 max-w-full mx-auto bg-gray-200 shadow-lg font-bebasneue">
-                <div className="flex items-center space-x-96">
-                    <h1>Footer</h1>
-                    <h1>Footer</h1>
-                    <h1>Made By <a href="https://www.mikelgaldos.com" target="_blank">Mikel Galdos</a></h1>
-                    <h1>Footer</h1>
-                    <h1>Footer</h1>
-
+            <footer className="text-center bg-gray-200">
+                <div className="p-6 max-w-full mx-auto bg-gray-200 shadow-lg font-bebasneue text-center">
+                    <div className="flex justify-center md:mr-auto">
+                        <h1>Made By <a href="https://www.mikelgaldos.com" target="_blank">Mikel Galdos</a></h1>
+                    </div>
                 </div>
-            </div>
+            </footer>
         </>
     )
 }
