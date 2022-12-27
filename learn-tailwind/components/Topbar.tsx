38ab58b5c246248838588c0387cda324a7fe5067
Mikel <mikelgaldosv@gmail.com>
2022-12-27 05:54:43 +0100

    twitter instagram and youtube logos added

--- learn-tailwind/components/Topbar.tsx
+++ learn-tailwind/components/Topbar.tsx
@@ -1,8 +1,12 @@
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
 export default function Topbar(): JSX.Element {
     return(
         <>
             <div className="p-6 max-w-full mx-auto bg-gray-200 shadow-lg flex items-center font-bebasneue">
-                <div className="ml-auto">
+                <div className="mr-auto">
                     <ul className="flex items-center space-x-4">
                         <li>
                             <a className="mr-3" href="/">Home</a>
@@ -18,6 +22,19 @@ export default function Topbar(): JSX.Element {
                         </li>
                         <li>
                             <a href="Contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="ml-auto">
+                    <ul className="flex items-center space-x-4">
+                        <li>
+                            <a target="_blank" href="https://twitter.com/FrankieLollia"><TwitterIcon/></a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="https://www.youtube.com/@frankielollia"><YouTubeIcon/></a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="https://www.instagram.com/frankielollia/?hl=en"><InstagramIcon/></a>
                         </li>
                     </ul>
                 </div>
