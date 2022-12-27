efdc1de654c2cc052ce9004113add01f775943c8
Mikel <mikelgaldosv@gmail.com>
2022-12-28 00:47:15 +0100

    a tag changed for next link

--- learn-tailwind/components/Topbar.tsx
+++ learn-tailwind/components/Topbar.tsx
@@ -2,6 +2,8 @@ import YouTubeIcon from '@mui/icons-material/YouTube';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 
+import Link from 'next/link';
+
 export default function Topbar(): JSX.Element {
     return(
         <>
@@ -9,32 +11,32 @@ export default function Topbar(): JSX.Element {
                 <div className="mr-auto">
                     <ul className="flex items-center space-x-4">
                         <li>
-                            <a className="mr-3" href="/">Home</a>
+                            <Link className="mr-3" href="/">Home</Link>
                             <span className="border-r border border-slate-900"></span>
                         </li>
                         <li>
-                            <a className="mr-3" href="#About">About</a>
+                            <Link className="mr-3" href="#About">About</Link>
                             <span className="border-r border border-slate-900"></span>
                         </li>
                         <li>
-                            <a className="mr-3" href="#Portfolio">Portfolio</a>
+                            <Link className="mr-3" href="#Portfolio">Portfolio</Link>
                             <span className="border-r border border-slate-900"></span>
                         </li>
                         <li>
-                            <a href="Contact">Contact</a>
+                            <Link href="Contact">Contact</Link>
                         </li>
                     </ul>
                 </div>
                 <div className="ml-auto">
                     <ul className="flex items-center space-x-4">
                         <li>
-                            <a target="_blank" href="https://twitter.com/FrankieLollia"><TwitterIcon className='hover:text-blue-300'/></a>
+                            <Link target="_blank" href="https://twitter.com/FrankieLollia"><TwitterIcon className='hover:text-blue-300'/></Link>
                         </li>
                         <li>
-                            <a target="_blank" href="https://www.youtube.com/@frankielollia"><YouTubeIcon className='hover:text-red-500'/></a>
+                            <Link target="_blank" href="https://www.youtube.com/@frankielollia"><YouTubeIcon className='hover:text-red-500'/></Link>
                         </li>
                         <li>
-                            <a target="_blank" href="https://www.instagram.com/frankielollia/?hl=en"><InstagramIcon className='hover:text-rose-400'/></a>
+                            <Link target="_blank" href="https://www.instagram.com/frankielollia/?hl=en"><InstagramIcon className='hover:text-rose-400'/></Link>
                         </li>
                     </ul>
                 </div>
