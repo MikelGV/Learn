d8c35468a1a09f7857b30abf7a10f217731e8299
Mikel <mikelgaldosv@gmail.com>
2022-12-28 03:49:43 +0100

    image test

--- learn-tailwind/pages/index.tsx
+++ learn-tailwind/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       </div>
 
       <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-        <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="/img/erin-lindford.jpg" alt="Woman's Face"/>
+        <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="/images/Frankie.png" alt="Woman's Face"/>
         <div className="text-center space-y-2 sm:text-left">
           <div className="space-y-0.5">
             <p className="text-lg text-black font-semibold">
