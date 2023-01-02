aaf9face2bcd02099f09d0aba90128e00de8a75e
Mikel <mikelgaldosv@gmail.com>
2023-01-02 12:59:10 +0100

    font link changed

--- learn-tailwind/components/Layout.tsx
+++ learn-tailwind/components/Layout.tsx
@@ -2,28 +2,15 @@ import Head from "next/head";
 import Topbar from "./Topbar";
 import Footer from "./Footer";
 
-const siteTitle: string = 'Tailwind test website'
 
 export default function Layout({children, home}: {children: any, home: boolean}) {
     return(
         <div>
             <Head>
                 <link rel="icon" href="/favicon.ico" />
-                <meta
-                name="description"
-                content="Learn how to use tailwind"
-                />
-                <meta
-                property="og:image"
-                content={`https://og-image.vercel.app/${encodeURI(
-                    siteTitle,
-                )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-                />
-                <meta name="og:title" content={siteTitle} />
-                <meta name="twitter:card" content="summary_large_image" />
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-                <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet"/> 
+                <style>
+                    @import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+                </style>
             </Head>
 
             <header>
