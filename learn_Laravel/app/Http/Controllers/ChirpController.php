3b3f59b23e8b3cca3e969a09d0ac8bbb7e6309d0
Mikel <mikelgaldosv@gmail.com>
2023-02-08 19:48:41 +0100

    edit controller/route/index

--- learn_Laravel/app/Http/Controllers/ChirpController.php
+++ learn_Laravel/app/Http/Controllers/ChirpController.php
@@ -70,6 +70,11 @@ class ChirpController extends Controller
     public function edit(Chirp $chirp)
     {
         //
+        $this->authorize('update', $chirp);
+
+        return view('chirps.edit', [
+            'chirp' => $chirp,
+        ]);
     }
 
     /**
@@ -82,6 +87,14 @@ class ChirpController extends Controller
     public function update(Request $request, Chirp $chirp)
     {
         //
+        $this->authorize('update', $chirp);
+
+        $validate = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+        $chirp->update($validate);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
