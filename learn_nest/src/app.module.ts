1773fd37f00a02b1a72e46e408553847c8d52529
Mikel <mikelgaldosv@gmail.com>
2023-09-07 08:49:52 +0200

    pg database connected

--- learn_nest/src/app.module.ts
+++ learn_nest/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      password: process.env.DB_PWORD,
+      username: process.env.DB_USERNAME,
+      database: process.env.DB_DB,
+      entities: [],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
