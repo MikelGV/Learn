d564cd278958708b02e6f892ae6538af5521b94d
Mikel <mikelgaldosv@gmail.com>
2023-09-07 08:53:57 +0200

    user resource created

--- learn_nest/src/app.module.ts
+++ learn_nest/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 import * as dotenv from 'dotenv';
 
@@ -18,7 +19,8 @@ dotenv.config();
       database: process.env.DB_DB,
       entities: [],
       synchronize: true,
-    })
+    }),
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
