faf131d66132bf1dd9fffe8792b9a5568c289470
Mikel <mikelgaldosv@gmail.com>
2023-09-20 19:02:38 +0200

    validation added to create-user.dto.ts

--- learn_nest/src/user/dto/create-user.dto.ts
+++ learn_nest/src/user/dto/create-user.dto.ts
@@ -1,7 +1,26 @@
-export class CreateUserDto {
-    username: string;
-    
-    email: string;
+import { IsEmail, IsNotEmpty, IsString, Matches, MinLength } from "class-validator";
 
+
+const pwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,20}$/;
+
+export class CreateUserDto {
+    @IsString()
+    @MinLength(3, {message: 'Username must have at least 3 characters'})
+    @IsNotEmpty()
+    username: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+  
+    @IsNotEmpty()
+    @Matches(pwordRegex, {
+        message: 
+        `Password must contain a Minimum 8 and Maximum 20 characters,
+        at least one uppercase letter,
+        one lowercase letter,
+        one number and
+        one special character`,
+    })
     password: string;
-}
+  }
