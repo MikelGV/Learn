c05b008f59f85070cf074c4e6602054c5117951c
Mikel <mikelgaldosv@gmail.com>
2023-09-20 07:35:17 +0200

    global pipes added to main

--- learn_nest/src/main.ts
+++ learn_nest/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as dotenv from "dotenv";
+import { ValidationPipe } from '@nestjs/common';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
 }
 bootstrap();
