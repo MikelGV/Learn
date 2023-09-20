9b4671a7e013e8ed66a2dbab3ff7f206385b554f
Mikel <mikelgaldosv@gmail.com>
2023-09-20 18:54:46 +0200

    app.useglobalpipes deleted

--- learn_nest/src/main.ts
+++ learn_nest/src/main.ts
@@ -8,7 +8,6 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
 }
 bootstrap();
