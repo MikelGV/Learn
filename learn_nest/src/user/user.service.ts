e4fb3c1021924e093b131d7dd847534af55db87c
Mikel <mikelgaldosv@gmail.com>
2023-09-22 15:45:51 +0200

    create user service added

--- learn_nest/src/user/user.service.ts
+++ learn_nest/src/user/user.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {};
+  
+  createUser(createUserDto: CreateUserDto):Promise<User> {
+    const user: User = new User();
+    user.username = createUserDto.username;
+    user.email = createUserDto.email;
+    user.password = createUserDto.password;
+    return this.userRepository.save(user);
   }
 
   findAll() {
