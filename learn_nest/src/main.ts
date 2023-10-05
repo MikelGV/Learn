d6d33bf8ce43c981605a2f1ca80414a405bd664d
Mikel <mikelgaldosv@gmail.com>
2023-10-05 21:16:53 +0200

    using golbaly validation pipe

--- learn_nest/src/main.ts
+++ learn_nest/src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as dotenv from "dotenv";
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from './common/pipes/validation/validation.pipe';
+
+
+
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
 }
 bootstrap();
