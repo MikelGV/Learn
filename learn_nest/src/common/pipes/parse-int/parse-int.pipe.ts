ee2bbea272bc438ee4f024a5fd74e4e35e802446
Mikel <mikelgaldosv@gmail.com>
2023-10-11 12:15:29 +0200

    custom parseint checks if value is empty

--- learn_nest/src/common/pipes/parse-int/parse-int.pipe.ts
+++ learn_nest/src/common/pipes/parse-int/parse-int.pipe.ts
@@ -1,8 +1,12 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 
 @Injectable()
 export class ParseIntPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
-    return value;
+    const val = parseInt(value, 10);
+    if (isNaN(val)) {
+      throw new BadRequestException('Validation Failed!');
+    }
+    return val;
   }
 }
