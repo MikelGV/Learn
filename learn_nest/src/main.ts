95d027843fa84958d66bd9e57688af37273d47a2
Mikel <mikelgaldosv@gmail.com>
2023-10-13 16:09:08 +0200

    use global guards new roles guard

--- learn_nest/src/main.ts
+++ learn_nest/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as dotenv from "dotenv";
 import { ValidationPipe } from './common/pipes/validation/validation.pipe';
+import { RolesGuard } from './common/guards/roles/roles.guard';
 
 
 
@@ -12,6 +13,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalGuards(new RolesGuard());
   await app.listen(process.env.PORT);
 }
 bootstrap();
