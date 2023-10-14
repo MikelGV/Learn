3c6d5971ac09ccd6b670049d8128defe27ff2bc2
Mikel <mikelgaldosv@gmail.com>
2023-10-14 10:12:03 +0200

    roles guard logic added

--- learn_nest/src/common/guards/roles/roles.guard.ts
+++ learn_nest/src/common/guards/roles/roles.guard.ts
@@ -1,11 +1,30 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+import { Roles } from 'src/common/decorators/roles/roles.decorator';
+import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+  constructor(private reflector: Reflector) {}
+
+  async matchRoles(roles: string[], userRole: User['roles']) {
+    let match = false;
+
+    if (roles.indexOf(userRole) > -1) {
+      match = true;
+    }
+
+    return match
+  }
+
+  async canActivate(context: ExecutionContext) {
+    const roles = this.reflector.get(Roles, context.getHandler());
+    if (!roles) {
+      return true
+    }
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    return this.matchRoles(roles, user.roles);
+
   }
 }
