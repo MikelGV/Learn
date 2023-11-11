4d235689af39e51d8d2b37a3b2fcad99474a9179
Mikel <mikelgaldosv@gmail.com>
2023-11-11 07:42:02 +0100

    unnecessary spaces removed from the code

--- learn_nest/src/user/user.service.ts
+++ learn_nest/src/user/user.service.ts
@@ -7,18 +7,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UserService {
-
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
-  ) {};
-  
+  ) {}
   createUser(createUserDto: CreateUserDto):Promise<User> {
     const user: User = new User();
     user.username = createUserDto.username;
     user.email = createUserDto.email;
     user.password = createUserDto.password;
     user.roles = createUserDto.roles;
-    
     return this.userRepository.save(user);
   }
 
