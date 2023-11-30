7dc58fdbbd069f88162a8eb4ed619e6d71cc62da
Mikel <mikelgaldosv@gmail.com>
2023-12-01 00:54:42 +0100

    db connection updated

--- learn_nest/src/config/database/database.service.ts
+++ learn_nest/src/config/database/database.service.ts
@@ -3,25 +3,24 @@ import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 
 @Injectable({ scope: Scope.DEFAULT })
-export class DatabaseService implements TypeOrmOptionsFactory { // use ,onModuleInit
-    // onModuleInit() {
-    //     console.log('this starts');
-    // }
+export class DatabaseService implements TypeOrmOptionsFactory {
+  // use ,onModuleInit
+  // onModuleInit() {
+  //     console.log('this starts');
+  // }
 
-    constructor(private configService: ConfigService) {}
+  constructor(private configService: ConfigService) {}
 
-    createTypeOrmOptions(): TypeOrmModuleOptions {
-        return {
-            type: 'postgres',
-            host: process.env.DB_HOST,
-            port: parseInt(process.env.DB_PORT),
-            password: process.env.DB_PWORD,
-            username: process.env.DB_USERNAME,
-            database: process.env.DB_DB,
-            entities: [
-                "dist/**/*.entity{.ts,.js}",
-            ],
-            synchronize: true,
-        };
-    }
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      password: process.env.DB_PWORD,
+      username: process.env.DB_USERNAME,
+      database: process.env.DB_DB,
+      entities: [`dist/**/*.entity{.ts,.js}`],
+      synchronize: true,
+    };
+  }
 }
