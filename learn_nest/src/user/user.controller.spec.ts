f85288743d86f9b560b30c83d1d65ae98810e183
Mikel <mikelgaldosv@gmail.com>
2023-11-30 03:08:37 +0100

    unit test added (fails on the result)

--- learn_nest/src/user/user.controller.spec.ts
+++ learn_nest/src/user/user.controller.spec.ts
@@ -1,20 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+
+const moduleMocker = new ModuleMocker(global);
 
 describe('UserController', () => {
   let controller: UserController;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
       providers: [UserService],
-    }).compile();
+    })
+      .useMocker((token) => {
+        const results = ['test1', 'test2'];
+        if (token === UserService) {
+          return { findAllUsers: jest.fn().mockResolvedValue(results) };
+        }
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token,
+          ) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
     controller = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findAll', () => {
+    it('should return an array of users', async () => {
+      const result = ['test'];
+      jest.spyOn(userService, 'findAllUsers').mockImplementation(() => result);
+
+      expect(await userService.findAllUsers()).toBe(result);
+    });
   });
 });
