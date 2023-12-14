85baf66796fe90d95df2e6e3b0c9c1ad36c533ca
MikelGv <mikelgaldosv@gmail.com>
2023-12-14 23:10:08 +0100

    first commit using nvim

--- learn_nest/src/common/interceptors/logging/logging.interceptor.ts
+++ learn_nest/src/common/interceptors/logging/logging.interceptor.ts
@@ -4,11 +4,11 @@ import { Observable, tap } from 'rxjs';
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('Before....');
-
+    console.log('BEFORE....');
+    
     const now = Date.now();
     return next.handle().pipe(
-      tap(() => console.log(`After.... ${Date.now() - now}ms`)),
+      tap(() => console.log(`AFTER.... ${Date.now() - now}ms`)),
     );
   }
 }
