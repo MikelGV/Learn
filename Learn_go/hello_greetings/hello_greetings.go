c156a6a6dd9e8aca7b5995f6ec380472c03937c1
MikelGv <mikelgaldosv@gmail.com>
2023-12-18 13:38:34 +0100

    How Error Handling works in go

--- Learn_go/hello_greetings/hello_greetings.go
+++ Learn_go/hello_greetings/hello_greetings.go
@@ -2,12 +2,26 @@ package main
 
 import (
     "fmt"
+    "log"
 
     "example.com/greetings"
 )
 
 func main() {
+    // Set properties of the predefined logger.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
     // Get a greeting message and print it.
-    message := greetings.Hello("Mikel")
+    //message := greetings.Hello("Mikel")
+    //fmt.Println(message)
+
+    // Request a greeting message.
+    message, err := greetings.Hello("")
+   // Look for non-nil error value
+    if err != nil {
+        log.Fatal(err)
+    }
+
     fmt.Println(message)
 }
