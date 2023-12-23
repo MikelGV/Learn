3b4f94f6b12bfa80f1dc1b28f94fb759bc6fa05f
MikelGv <mikelgaldosv@gmail.com>
2023-12-23 19:55:36 +0100

    validation pipe imported from nestjs/common

--- learn_nest/src/main.ts
+++ learn_nest/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from './common/pipes/validation/validation.pipe';
-
+//import {ValidationPipe} from '@nestjs/common'
 dotenv.config();
 
 async function bootstrap() {
