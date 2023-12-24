c45877e47c441a3f753df97b73a13d9fb8faf2ad
MikelGv <mikelgaldosv@gmail.com>
2023-12-24 07:16:37 +0100

    go values added

---
+++ Learn_go/Go-By-Example/values/values.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    // String
+    fmt.Println("Hello" + "World")
+    // Int and Floats
+    fmt.Println("1+1=", 1+1)
+    fmt.Println("7.0/3.0=", 7.0/3.0)
+    // Boolean
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
