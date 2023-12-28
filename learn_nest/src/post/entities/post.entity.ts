761920036ba2e06570ea0842cc926831cc624db8
MikelGv <mikelgaldosv@gmail.com>
2023-12-28 07:41:11 +0100

    post entity added

--- learn_nest/src/post/entities/post.entity.ts
+++ learn_nest/src/post/entities/post.entity.ts
@@ -1,1 +1,12 @@
-export class Post {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+export class Post {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({ type:"varchar", length: 50 })
+    title: string
+
+    @Column({ type:"varchar", length: 5000 })
+    description: string
+}
