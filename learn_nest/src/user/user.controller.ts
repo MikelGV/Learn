c8e691530bcf4a8285108de56db987b7c21a5fec
MikelGv <mikelgaldosv@gmail.com>
2023-12-28 06:49:41 +0100

    another way of using the validation in the controller

--- learn_nest/src/user/user.controller.ts
+++ learn_nest/src/user/user.controller.ts
@@ -7,6 +7,8 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  ParseArrayPipe,
+  Query,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -32,6 +34,12 @@ export class UserController {
   async findOne(@Param('id', ParseIntPipe) id: number) {
     return this.userService.viewUser(id);
   }
+  // This get is another way to parse ids
+ // findOne(@Query('id', new ParseArrayPipe({ items: Number, separator: ',' }))
+   //     id: number[]    
+    // ) {
+    //     return 'this action returns users by id'
+    // }
 
   @Patch(':id')
   update(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
