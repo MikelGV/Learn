15165a7113d0d4aa8309699a2e3f5fe7507392a9
MikelGv <mikelgaldosv@gmail.com>
2024-01-02 15:13:51 +0100

    sorting collections by something other than its natural order

---
+++ Learn_go/Go-By-Example/sortingByFunctions/go.mod
@@ -0,0 +1,3 @@
+module example.com/sortingByFunctions
+
+go 1.21.5
---
+++ Learn_go/Go-By-Example/sortingByFunctions/sortingByFunctions.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"cmp"
+	"fmt"
+	"slices"
+)
+
+func main() {
+    fmt.Println("this is sorting by functions")
+
+    fruits := []string{"peach", "banana", "kiwi"}
+    lenCmp := func(a,b string) int {
+        return cmp.Compare(len(a), len(b))
+    }
+
+    slices.SortFunc(fruits, lenCmp)
+    fmt.Println(fruits)
+
+    type Person struct {
+        name string
+        age int
+    }
+
+    people := []Person{
+        Person{name: "Jax", age: 37},
+        Person{name: "Tj", age: 25},
+        Person{name: "Alex", age: 72},
+    }
+
+    slices.SortFunc(people,
+        func(a, b Person) int {
+            return cmp.Compare(a.age, b.age)
+        })
+    fmt.Println(people)
+}
