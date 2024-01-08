7d69341030c9dca229d166cf694c3b2f9d6d1a74
MikelGv <mikelgaldosv@gmail.com>
2024-01-08 09:54:56 +0100

    Exec'ing processes example

---
+++ Learn_go/Go-By-Example/execing-processes/execing-processes.go
@@ -0,0 +1,35 @@
+// Sometimes we just want to completely replace the current Go process with
+// another (perhaps non-Go) one. To do this we'll use Go's implementation of the
+// classic exec function.
+package main
+
+import (
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+    // Go requires an absolute path to the binary we want to execute, so we'll
+    // use exec.LookPath() to find it (probably /bin/ls)
+    binary, lookErr := exec.LookPath("ls")
+    if lookErr != nil {
+        panic(lookErr)
+    }
+
+    // Exec requires args in slice form. We'll give ls a few common args. Note
+    // the first arg should be the program name.
+    args := []string{"ls", "-a", "-l", "-h"}
+
+    // Exec also needs a set of environment variables to use. Here we just
+    // provide our current environment.
+    env := os.Environ()
+
+    // Here is the actual syscall.Exec call. If this call is successful, the
+    // execution of our process will end here and be replaced by the /bin/ls -a -l -h
+    // process. If there is an error we'll get a return value.
+    execErr := syscall.Exec(binary, args, env)
+    if execErr != nil {
+        panic(execErr)
+    }
+}
