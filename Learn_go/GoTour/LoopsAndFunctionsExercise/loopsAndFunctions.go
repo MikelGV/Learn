319295d40052c4ee96dceeb1566546da6f9976c9
MikelGv <mikelgaldosv@gmail.com>
2024-01-09 15:01:38 +0100

    Error

--- Learn_go/GoTour/LoopsAndFunctionsExercise/loopsAndFunctions.go
+++ Learn_go/GoTour/LoopsAndFunctionsExercise/loopsAndFunctions.go
@@ -5,10 +5,10 @@ import "fmt"
 func Sqrt(x float64) float64 {
     if x < 0 {
         return -1
-    } else {
+    }
+    if x == 0 {
         return 0
     }
-
     z := x / 2
 
     for i := 0; i <= 1000; i++ {
